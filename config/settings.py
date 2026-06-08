7635b4a0681452be1cffa0e38935cdd3abc862bb
vishnu R <157986076+vishnu-R2005@users.noreply.github.com>
2026-06-08 14:11:56 +0530

    created a jwt authentication access and refresh tocken

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -106,16 +106,27 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
 
-        "DEFAULT_AUTHENTICATION_CLASSES": [
+    "DEFAULT_AUTHENTICATION_CLASSES": (
 
-        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
 
-    ],
-    "DEFAULT_PERMISSION_CLASSES": [
+    ),
+
+    "DEFAULT_PERMISSION_CLASSES": (
 
         "rest_framework.permissions.IsAuthenticated",
 
-    ]
+    )
+
+}
+
+SIMPLE_JWT = {
+
+    "ACCESS_TOKEN_LIFETIME":
+        timedelta(minutes=15),
+
+    "REFRESH_TOKEN_LIFETIME":
+        timedelta(days=7),
 
 }
 
